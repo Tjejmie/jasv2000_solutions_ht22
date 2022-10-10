0dcad934c163f5c8eb5c29a0c0688acd7e0e2f5e
Tjejmie <jamie_svanberg@hotmail.com>
2022-10-10 15:47:36 +0200

    Comments in code and report in readme added

--- Laboration_3/assignment.py
+++ Laboration_3/assignment.py
@@ -29,43 +29,30 @@ RESOURCES = Path(__file__).parent / "../_Resources/"
 
 def create_logger() -> logging.Logger:
     """Create and return logger object."""
-    print("entering function create_logger")
 
-
-
-    file_path = RESOURCES / 'ass3_log_conf.json'
-    with open(file_path, 'r') as file:
-        config = json.load(file)
-        logging.config.dictConfig(config)
-
-        #logger = logging.getLogger('ass_3_logger')
-        #logger.info('This is a test')
-
-
-    print("exiting function create_logger")
+    file_path = RESOURCES / 'ass3_log_conf.json'   # Create full filepath
+    with open(file_path, 'r') as file:   # Open file
+        logger = json.load(file)   # Create logger for jsonfile
+        logging.config.dictConfig(logger)   # Load logger configs from JSON
+    logger = logging.getLogger('ass_3_logger')   # Creating object of the logging
+    return logger
 
 def measurements_decorator(func):
     """Function decorator, used for time measurements."""
-
     @wraps(func)
     def wrapper(nth_nmb: int) -> tuple:
 
-        container = []
-
-        logger = logging.getLogger('ass_3_logger')
-        logger.info('Starting measurements...')
-        start = timeit.default_timer()
+        value = []
+        LOGGER.info('Starting measurements...')
+        start = timeit.default_timer()   # Start timer
         for i in reversed(range(nth_nmb +1)):  # +1 so it start at nth_nmb and not nth_nmb -1
-            result = func(i)
-            container.append(result)
-            if i % 5 == 0:
-                logger.debug('%s: %s', i, result)
+            result = func(i)   # Get fibonacci value from methods
+            value.append(result)   # Add fibonacci values to container
+            if i % 5 == 0:   # for each 5th iteration log information
+                LOGGER.debug('%s: %s', i, result)
 
-        duration = timeit.default_timer() - start
-
-
-        return duration, tuple(container)
-
+        duration = timeit.default_timer() - start   # Get duration
+        return duration, tuple(value)   # Return duration and container as tuple
 
     return wrapper
 
@@ -81,7 +68,6 @@ def fibonacci_iterative(nth_nmb: int) -> int:
     for __ in range(nth_nmb - 1):
         old, new = new, old + new
 
-
     return new
 
 
@@ -93,9 +79,7 @@ def fibonacci_recursive(nth_nmb: int) -> int:
     def fib(_n):
         return _n if _n <= 1 else fib(_n - 1) + fib(_n - 2)
 
-
     return fib(nth_nmb)
-
 
 @measurements_decorator
 def fibonacci_memory(nth_nmb: int) -> int:
@@ -103,12 +87,11 @@ def fibonacci_memory(nth_nmb: int) -> int:
 
     memory = {0: 0, 1: 1}
     def fib(_n):
-        if _n in memory:
+        if _n not in memory:   # Check if value doesn't exist in dict
+            memory[_n] = fib(_n - 1) + fib(_n - 2)  # If value doesn't exist its calculated
             return memory[_n]
-        memory[_n] = fib(_n - 1) + fib(_n - 2)
-        return memory[_n]
+        return memory[_n]   # Return value in dict
     return fib(nth_nmb)
-
 
 
 
@@ -134,32 +117,27 @@ def print_statistics(fib_details: dict, nth_value: int):
     line = '\n' + ("---------------" * 5)
 
     print(line)
-    print(f"DURATION FOR EACH APPROACH WITHIN INTERVAL: {nth_value}-0".center(75)+f"{line}")
+    print(f"DURATION FOR EACH APPROACH WITHIN INTERVAL: {nth_value}-0".center(75)+f"{line}")   # Print line and header
 
     values = ['Seconds', 'Milliseconds', 'Microseconds', 'Nanoseconds']
-    print(f"{values[0].rjust(27)}{values[1].rjust(16)}{values[2].rjust(16)}{values[3].rjust(16)}")
-    for key, val in fib_details.items():
-        duration = (val[0])
-        sec = duration_format(duration, values[0])
-        millisec = duration_format(duration, values[1])
-        microsec = duration_format(duration, values[2])
-        nanosec = duration_format(duration, values[3])
+    print(f"{values[0].rjust(27)}{values[1].rjust(16)}{values[2].rjust(16)}{values[3].rjust(16)}")   # Print column headers
+    for key, val in fib_details.items():   # Get data from fib_details and declare it as key and val
+        duration = (val[0])   # Get duration
+        sec = duration_format(duration, values[0])   # Get seconds from duration_format
+        millisec = duration_format(duration, values[1])   # Get milliseconds from duration_format
+        microsec = duration_format(duration, values[2])   # Get microseconds from duration_format
+        nanosec = duration_format(duration, values[3])   # Get nanoseconds from duration_format
         print(f"{key.title().ljust(20)}{sec.rjust(0)}{millisec.rjust(16)}{microsec.rjust(16)}{nanosec.rjust(16)}")
 
 def write_to_file(fib_details: dict):
     """Function to write information to file."""
 
     for key, val in fib_details.items():   # Get data from fib_details and declare it as key and val
-        result = key.replace(' ', '_') + '.text'   # Modify name for textfiles
-        file_path = RESOURCES / result
+        file_path = RESOURCES / (key.replace(' ', '_') + '.txt')   # Modify name for textfiles
         f = open(f"{file_path}", "w+")   # Create new textfiles
-        value = (val[1])   # Get value from fib_details
-        newValue = reversed(value)   # Reverse list of values so it starts at 0
-        #result = tuple(result)
+        newValue = reversed(val[1])   # Get values from fib_details and reverse list
         seqAndValue = tuple(zip(itertools.count(), newValue))   # Add sequence to tuple of values
-
         result = reversed(seqAndValue)  # Reverse tuple of sequence and value
-        #result = tuple(result)
         for data in result:   # Get each item in tuple
             f.write("%s: %s \n" % data)   # Write item in file
 
